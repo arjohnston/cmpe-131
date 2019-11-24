5fbb72dc8bcc3394534af16e6fca1c9d2338bdff
Andrew Johnston <arjohnston3@gmail.com>
2019-11-24 12:50:49 -0800

    refactor chart to account for multiple lines

--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -3,56 +3,145 @@ import * as d3 from 'd3'
 
 import './style.css'
 
+// https://github.com/sxywu/react-d3-example/blob/master/src/visualizations/LineChart.js
+
+const margin = 20
+
+const colors = ['#2887d6', '#13c16f']
+
 class LineChart extends Component {
-  render () {
-    const { data, width, height, margin } = this.props
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      xAxis: null,
+      yAxis: null
+    }
+  }
+
+  componentDidMount () {
+    this.setAxises()
+  }
+
+  getTimeFormat () {
+    if (!this.props.timeFormat) return d3.timeFormat('%b')
+
+    switch (this.props.timeFormat) {
+      case 'day':
+        return d3.timeFormat('%d')
+
+      case 'week':
+        return d3.timeFormat('%a %d')
+
+      default:
+        // month
+        return d3.timeFormat('%b')
+    }
+  }
+
+  getYAxisMax () {
+    const { data } = this.props
+    if (!data) return
+
+    if (Object.keys(data[0]).length <= 2) {
+      return d3.max(data, d => d[Object.keys(data[0])[1]])
+    } else {
+      let max = 0
+
+      for (let i = 1; i <= Object.keys(data[0]).length; i++) {
+        const newMax = d3.max(data, d => d[Object.keys(data[0])[i]])
+
+        if (newMax > max) max = newMax
+      }
+
+      return max
+    }
+  }
+
+  setAxises () {
+    const { data, width, height } = this.props
+
+    const w = width - 2 * margin
+    const h = height - 2 * margin
+
+    const xAxis = d3
+      .scaleTime()
+      .domain(d3.extent(data, d => new Date(d.date)))
+      .range([margin + 15, w])
+
+    const yAxis = d3
+      .scaleLinear()
+      .domain([0, this.getYAxisMax()])
+      .range([h, margin])
+
+    this.setState({
+      xAxis: xAxis,
+      yAxis: yAxis
+    })
+  }
+
+  renderLines () {
+    const lines = []
+    const { data } = this.props
+
+    const line = d3
+      .line()
+      .x(data => this.state.xAxis(new Date(data.date)))
+      .y(data => this.state.yAxis(data.y))
+
+    for (let i = 1; i < Object.keys(data[0]).length; i++) {
+      const dataArr = []
+
+      data.forEach(dataPoint => {
+        const dataObj = {}
+        dataObj.date = dataPoint.date
+
+        dataObj.y = dataPoint[Object.keys(dataPoint)[i]]
+
+        dataArr.push(dataObj)
+      })
+
+      lines.push(line(dataArr))
+    }
+
+    return lines
+  }
+
+  renderChart () {
+    const { width, height } = this.props
+    const { xAxis, yAxis } = this.state
 
     const h = height - 2 * margin
     const w = width - 2 * margin
 
     // number formatter
-    const xFormat = d3.timeFormat('%b')
-    const yFormat = d3.format('.2')
+    const xAxisFormat = this.getTimeFormat()
+    const yAxisFormat = d3.format('.4')
 
-    // x scale
-    const x = d3
-      .scaleTime()
-      .domain(d3.extent(data, d => new Date(d.a))) // domain: [min,max] of a
-      .range([margin, w])
+    if (!xAxis || !yAxis) return
 
-    // y scale
-    const y = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, d => d.b)]) // domain [0,max] of b (start from 0)
-      .range([h, margin])
-
-    // line generator: each point is [x(d.a), y(d.b)] where d is a row in data
-    // and x, y are scales (e.g. x(10) returns pixel value of 10 scaled by x)
-    const line = d3
-      .line()
-      .x(d => x(new Date(d.a)))
-      .y(d => y(d.b))
-    // .curve(d3.curveCatmullRom.alpha(0.5)) // curve line
-
-    const xTicks = x.ticks(6).map((d, i) =>
-      x(d) > margin && x(d) < w ? (
-        <g transform={`translate(${x(d)},${h + margin})`} key={i}>
-          <text>{xFormat(d)}</text>
+    const xTicks = xAxis.ticks(6).map((dataPoint, index) =>
+      xAxis(dataPoint) > margin && xAxis(dataPoint) < w ? (
+        <g
+          transform={`translate(${xAxis(dataPoint)},${h + margin})`}
+          key={index}
+        >
+          <text>{xAxisFormat(dataPoint)}</text>
           <line x1='0' x2='0' y1='0' y2='5' transform='translate(0,-20)' />
         </g>
       ) : null
     )
 
-    const yTicks = y.ticks(5).map((d, i) =>
-      y(d) > 10 && y(d) < h ? (
-        <g transform={`translate(${margin}, ${y(d)})`} key={i}>
-          <text x='-12' y='5'>
-            {yFormat(d)}
+    const yTicks = yAxis.ticks(5).map((dataPoint, index) =>
+      yAxis(dataPoint) > 10 && yAxis(dataPoint) < h ? (
+        <g transform={`translate(${margin}, ${yAxis(dataPoint)})`} key={index}>
+          <text x='-5' y='5'>
+            {yAxisFormat(dataPoint)}
           </text>
-          <line x1='0' x2='5' y1='0' y2='0' transform='translate(-5,0)' />
+          <line x1='15' x2='20' y1='0' y2='0' transform='translate(-5,0)' />
           <line
             className='gridline'
-            x1='0'
+            x1='15'
             x2={w - margin}
             y1='0'
             y2='0'
@@ -63,14 +152,35 @@ class LineChart extends Component {
     )
 
     return (
-      <div className='line-chart'>
-        <svg width={width} height={height}>
-          <line className='axis' x1={margin} x2={w} y1={h} y2={h} />
-          <line className='axis' x1={margin} x2={margin} y1={margin} y2={h} />
-          <path d={line(data)} fill='none' stroke='blue' />
-          <g className='axis-labels'>{xTicks}</g>
-          <g className='axis-labels'>{yTicks}</g>
-        </svg>
+      <svg width={width} height={height}>
+        <line className='axis' x1={margin + 15} x2={w} y1={h} y2={h} />
+        <line
+          className='axis'
+          x1={margin + 15}
+          x2={margin + 15}
+          y1={margin}
+          y2={h}
+        />
+        {this.renderLines().map((line, index) => {
+          return <path d={line} stroke={colors[index]} key={index} />
+        })}
+        <g className='axis-labels'>{xTicks}</g>
+        <g className='axis-labels'>{yTicks}</g>
+      </svg>
+    )
+  }
+
+  render () {
+    return (
+      <div
+        style={{
+          width: `${this.props.width + 38}px`,
+          minHeight: `${this.props.height}px`,
+          margin: '12px'
+        }}
+      >
+        <h2>{this.props.title}</h2>
+        <div className='line-chart'>{this.renderChart()}</div>
       </div>
     )
   }
--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -8,6 +8,9 @@ import LineChart from '../components/LineChart/LineChart'
 // resize width / height
 // Should always keep 6:4 aspect ratio
 
+// get profile info & display
+// get daily entries info & build 3x chart data
+
 // Overview of their profile:
 // Weight
 // Gender
@@ -28,15 +31,6 @@ import LineChart from '../components/LineChart/LineChart'
 
 const width = 600
 const height = 400
-const margin = 20
-
-const data = [
-  { a: '1/1/2017', b: 3 },
-  { a: '2/2/2017', b: 6 },
-  { a: '3/3/2017', b: 2 },
-  { a: '4/4/2017', b: 12 },
-  { a: '5/5/2017', b: 8 }
-]
 
 export default class extends Component {
   constructor (props) {
@@ -44,36 +38,60 @@ export default class extends Component {
 
     this.state = {
       token: '',
-      data: [
+      chartOne: [
         {
           date: '1/1/2017',
-          calories: 540,
-          weight: 180,
-          exercise: 30,
+          calories: 540
+        },
+        {
+          date: '2/5/2017',
+          calories: 1024
+        },
+        {
+          date: '3/10/2017',
+          calories: 512
+        },
+        {
+          date: '4/21/2017',
+          calories: 618
+        }
+      ],
+      chartTwo: [
+        {
+          date: '1/1/2017',
+          exercise: 30
+        },
+        {
+          date: '2/5/2017',
+          exercise: 25
+        },
+        {
+          date: '3/10/2017',
+          exercise: 0
+        },
+        {
+          date: '4/21/2017',
+          exercise: 45
+        }
+      ],
+      chartThree: [
+        {
+          date: '1/1/2017',
           bloodPressure: 180,
           heartRate: 105
         },
         {
-          date: '1/5/2017',
-          calories: 1024,
-          weight: 174,
-          exercise: 25,
+          date: '2/5/2017',
           bloodPressure: 170,
           heartRate: 95
         },
         {
-          date: '1/10/2017',
-          calories: 512,
-          weight: 170,
-          exercise: 0,
+          date: '3/10/2017',
           bloodPressure: 160,
           heartRate: 100
         },
         {
-          date: '1/21/2017',
-          calories: 618,
-          weight: 175,
-          exercise: 45,
+          date: '4/21/2017',
           bloodPressure: 175,
           heartRate: 110
         }
@@ -105,18 +123,40 @@ export default class extends Component {
         <div
           style={{
             width: `${width + 38}px`,
-            minHeight: `${height + 41}px`,
-            background: 'white',
-            padding: '18px',
-            border: '1px solid #DDD',
             margin: '12px'
           }}
         >
-          Profile Snapshot
+          <h2>Profile Snapshot</h2>
+          <div
+            style={{
+              width: '100%',
+              minHeight: `${height + 41}px`,
+              background: 'white',
+              padding: '18px',
+              border: '1px solid #DDD'
+            }}
+          >
+            profile....
+          </div>
         </div>
-        <LineChart data={data} width={width} height={height} margin={margin} />
-        <LineChart data={data} width={width} height={height} margin={margin} />
-        <LineChart data={data} width={width} height={height} margin={margin} />
+        <LineChart
+          data={this.state.chartOne}
+          width={width}
+          height={height}
+          title='Calories'
+        />
+        <LineChart
+          data={this.state.chartTwo}
+          width={width}
+          height={height}
+          title='Exercise'
+        />
+        <LineChart
+          data={this.state.chartThree}
+          width={width}
+          height={height}
+          title='Blood Pressure & Heart Rate'
+        />
       </div>
     )
   }
